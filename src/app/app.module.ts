b73558d61ed95af65436bf9fb5a53de84e4d2995
Edis Jasarevic <e.jasarevic03@gmail.com>
2023-02-22 12:37:01 +0100

    basic layout changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './public/components/footer/footer.component';
+import { HeaderComponent } from './public/components/header/header.component';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        
+        
+    ]
 })
 export class AppModule { }
